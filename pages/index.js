4bcee8b05b6aacc9e18a9b137395ba02b802bd28
kumartul <kumartul001@gmail.com>
2022-08-08 00:59:38 +0530

    feat: create musicode

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,28 @@
 import Head from 'next/head';
 
+import Header from '../components/Header';
+import Cards from '../components/Cards';
+import Footer from '../components/Footer';
+import ThemeToggler from '../components/ThemeToggler';
+
 const Home = () => {
 	return (
 		<div>
 			<Head>
-				<title>Musicode</title>
-				
+				<title>Musicode - Music 🎵 to keep you on while coding 🧑‍💻</title>
+
 				<meta name="description" content="Music 🎵 to keep you on while coding 🧑‍💻" />
 
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" href="/favicon.webp" />
 			</Head>
+
+			<Header />
+
+			<Cards />
+
+			<Footer />
+
+			<ThemeToggler />
 		</div>
 	);
 }
