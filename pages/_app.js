1336f4a131434afc854bafbfcad652ba3989bb57
kumartul <kumartul001@gmail.com>
2022-08-07 12:28:33 +0530

    feat: dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from 'next-themes';
+
+const MyApp = ({ Component, pageProps }) => {
+	return (
+		<ThemeProvider attribute="class">
+			<Component {...pageProps} />
+		</ThemeProvider>
+	)
 }
 
-export default MyApp
+export default MyApp;
