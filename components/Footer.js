54cc7dd4c332e68a896e5125e0068d0b231f434c
kumartul <kumartul001@gmail.com>
2022-08-08 00:56:59 +0530

    feat: responsive design

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
 	return (
-		<footer className="bg-white shadow-lg mt-24 p-4 flex items-center justify-between dark:bg-[#1a1a1a]">
+		<footer className="bg-white shadow-lg mt-24 p-4 flex flex-col space-y-2 md:lg:flex-row md:lg:space-y-0 items-center justify-between dark:bg-[#1a1a1a]">
 			<div className="text-md dark:text-gray-300">
 				⚙️ Built by <a
 					href="https://www.github.com/kumartul"
