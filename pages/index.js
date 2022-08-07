ec6d944c00cd4eb26bab7836be10e1cd2f7f3ee7
kumartul <kumartul001@gmail.com>
2022-08-08 01:10:23 +0530

    feat: improve seo

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,33 @@ const Home = () => {
 	return (
 		<div>
 			<Head>
-				<title>Musicode - Music 🎵 to keep you on while coding 🧑‍💻</title>
-
+				{/* Required meta tags */}
+				<meta charset="UTF-8" />
+				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="description" content="Music 🎵 to keep you on while coding 🧑‍💻" />
+				<meta name="keywords" content="coding, programming, music, songs, sound, audio, lofi, mood, battle, beach, birds, boat, books, campfire, chill, clock, drinking, dripping water, water, energy, war bgm, farm, fireworks, flute, footsteps, forest, grind, keyboard, leaves, mouse, polozhenie, rain, retro, river, road, bhool bhulaiyaa title track, rocket, snow, storm, thunder, train, victory, wildcard, ed sheeran, ed, sheeran, shape of you, shape, of, you, atul, kumar, atul kumar, kumartul" />
+				<meta name="author" content="Atul Kumar" />
+
+				{/* Open Graph Meta Tags */}
+				<meta property="og:title" content="Musicode 🎵" />
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://musicode.vercep.app" />
+				<meta property="og:image" content="https://musicode.vercel.app/mockup.png" />
+				<meta property="og:description" content="Music 🎵 to keep you on while coding 🧑‍💻" />
+				<meta property="og:site_name" content="Musicode" />
+				<meta property="og:locale" content="en_US" />
+
+				{/* Twitter Meta Tags */}
+				<meta property="twitter:card" content="summary" />
+				<meta property="twitter:title" content="Musicode" />
+				<meta property="twitter:description" content="Music 🎵 to keep you on while coding 🧑‍💻" />
+				<meta property="twitter:image" content="https://musicode.vercel.app/mockup.png" />
+				<meta property="twitter:image:alt" content="Musicode Home Page" />
+				<meta property="twitter:site" content="@kumartul001" />
+				<meta property="twitter:creator" content="@kumartul001" />
+
+				<title>Musicode - Music 🎵 to keep you on while coding 🧑‍💻</title>
 
 				<link rel="icon" href="/favicon.webp" />
 			</Head>
