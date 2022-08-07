f0e1e61e549b9c0b55555f543169c90735566496
kumartul <kumartul001@gmail.com>
2022-08-07 12:07:19 +0530

    feat: handle dark mode using class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	mode: 'jit',
+	darkMode: 'class',
 	content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {},
